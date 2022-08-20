19057c33b875470e06b42b5c5beaed6e001c1d0b
EquipoLandDom <misionlanddom@gmail.com>
2022-08-19 20:07:26 -0500

    Proyecto

---
+++ src/main/java/com/example/misionTicApp/MisionTicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.misionTicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisionTicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisionTicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/misionTicApp/MisionTicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.misionTicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisionTicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
